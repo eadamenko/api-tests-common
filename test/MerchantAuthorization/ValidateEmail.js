cb6b0add272c8832690e98c4b9d9555cb560ba93
eugene.adamenko <eugene.adamenko@intellectsoft.net>
2017-09-13 13:51:47 +0300

    Tests for Validate email - FV-192 Refactoring Login tests Test for Login

--- test/MerchantAuthorization/ValidateEmail.js
+++ test/MerchantAuthorization/ValidateEmail.js
@@ -1,11 +1,51 @@
 describe('Validate Email', function () {
     describe('When new user submit registration form', function () {
-        it('should be an error if there is no email', function (done) {
+        it('should be an error if email field not sended', function (done) {
             chai.request(app)
                 .post('/validate/email') //empty email
                 .end(function (err, res) {
                     res.should.have.status(422);
                     res.body.should.have.property('message').equal('Invalid data in request body');
+                    res.body.should.have.property('errors');
+                    done();
+                })
+        });
+
+        it('should be an error if invalid request payload', function (done) {
+            chai.request(app)
+                .post('/validate/email')
+                .send({
+                    emaill: 'someemail@example.com' //invalid field name
+                })
+                .end(function (err, res) {
+                    res.should.have.status(422);
+                    res.body.should.have.property('message').equal('Invalid data in request body');
+                    res.body.should.have.property('errors');
+                    done();
+                })
+        });
+
+        it('should be an error if account with such email already exist', function (done) {
+            chai.request(app)
+                .post('/validate/email')
+                .send({
+                    email: emailMerchant //email already exist
+                })
+                .end(function (err, res) {
+                    res.should.have.status(409);
+                    res.body.should.have.property('message').equal('An account with this email address already exists');
+                    done();
+                })
+        });
+
+        it('should allow unique email', function (done) {
+            chai.request(app)
+                .post('/validate/email')
+                .send({
+                    email: faker.internet.email()  //unique email
+                })
+                .end(function (err, res) {
+                    res.should.have.status(200);
                     done();
                 })
         });
--- test/test.bootstrap.js
+++ test/test.bootstrap.js
@@ -3,6 +3,8 @@ process.env.NODE_ENV='test'; // any more test specific init code
 global.chai = require('chai');
 global.chaiHttp = require('chai-http');
 global.should  = chai.should();
+global.expect = chai.expect();
+global.faker = require('faker');
 
 require('dotenv').config();
 
@@ -13,6 +15,7 @@ emailMerchant = process.env.LOGIN_MERCHANT;
 passMerchant = process.env.PASS_MERCHANT;
 emailAdmin = process.env.LOGIN_AMDIN;
 passAdmin = process.env.PASS_ADMIN;
+
 
 
 
